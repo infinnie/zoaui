2401cf26d0275d815ec1384ca7a6dcc23c083641
infinnie <w4ergf@qq.com>
2015-08-07 23:11:19 +0800

    Modified styles

--- js/toggle.js
+++ js/toggle.js
@@ -1,7 +1,20 @@
 ﻿/// <reference path="jquery/jquery-1.10.2.min.js"/>
 jQuery(function ($) {
     $("[data-zo-toggle]").click(function () {
-        $($(this).attr("data-zo-toggle")).toggleClass("zojs--on");
+        var el = $($(this).attr("data-zo-toggle"));
+        if (el.hasClass("zojs--on")) {
+            el.removeClass("zojs--on").trigger("close.zo");
+        } else {
+            el.addClass("zojs--on").trigger("open.zo");
+        }
+        return false;
+    });
+    $("[data-zo-open]").click(function () {
+        $($(this).attr("data-zo-open")).addClass("zojs--on").trigger("open.zo");
+        return false;
+    });
+    $("[data-zo-close]").click(function () {
+        $($(this).attr("data-zo-close")).removeClass("zojs--on").trigger("close.zo");
         return false;
     });
     $(window).on("keyup", function (e) {
