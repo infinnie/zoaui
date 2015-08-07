77fe4211539715c170f170a969b1df0458e51868
infinnie <w4ergf@qq.com>
2015-08-07 09:15:22 +0800

    modified dialog

--- js/nav.js
+++ js/nav.js
@@ -1,30 +1,10 @@
 ﻿/// <reference path="jquery/jquery-1.10.2.min.js"/>
 (function ($) {
-    var s, _CALLBACK = function ($) {
-        // Now we do something
-        $(function () {
-            $(document).on("click", "[data-zo-toggle=navbar]", function () {
-                $($(this).attr("data-zo-for")).toggleClass("zojs-navbar--on");
-                return false;
-            });
+    // Later release the polyfill in case jQuery is not yet loaded
+    $(function () {
+        $(document).on("click", "[data-zo-navbar]", function () {
+            $($(this).attr("data-zo-navbar")).toggleClass("zojs-navbar--on");
+            return false;
         });
-    };
-    if (!$) {
-        s = document.createElement("script");
-        s.src = "/js/jquery/jquery-1.10.2.min.js";
-        if ("onload" in s) {
-            s.onload = function () {
-                _CALLBACK.call(window, jQuery);
-            };
-        } else {
-            s.onreadystatechange = function () {
-                if (s.readyState == "loaded" || s.readyState == "complete") {
-                    _CALLBACK.call(window, jQuery);
-                }
-            };
-        }
-        document.getElementsByTagName("head")[0].appendChild(s);
-        return;
-    }
-    _CALLBACK.call(window, $);
+    });
 })(window.jQuery);
---
+++ js/toggle.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="jquery/jquery-1.10.2.min.js"/>
+jQuery(function ($) {
+    $("[data-zo-toggle]").click(function () {
+        $($(this).attr("data-zo-toggle")).toggleClass("zojs--on");
+        return false;
+    });
+    $(window).on("keyup", function (e) {
+        if (e.keyCode === 27) {
+            $(".zojs--on").removeClass("zojs--on");
+        }
+    });
+});
