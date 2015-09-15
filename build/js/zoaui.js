6b67efa2b170404fc4a34a8624705cbaaf214829
infinnie <w4ergf@qq.com>
2015-09-16 00:37:35 +0800

    Build using Gulp

--- build/js/zoaui.js
+++ build/js/zoaui.js
@@ -1,1 +1,40 @@
-(function(n){n(function(){n(document).on("click","[data-zo-navbar]",function(){return n(n(this).attr("data-zo-navbar")).toggleClass("zojs-navbar--on"),!1})})})(window.jQuery),function(n){n(function(){n(document).on("click","[data-zo-toggle]",function(){var t=n(n(this).attr("data-zo-toggle"));return t.hasClass("zojs--on")?t.removeClass("zojs--on").trigger("close.zo"):t.addClass("zojs--on").trigger("open.zo"),!1});n(document).on("click","[data-zo-open]",function(){return n(n(this).attr("data-zo-open")).addClass("zojs--on").trigger("open.zo"),!1});n(document).on("click","[data-zo-close]",function(){return n(n(this).attr("data-zo-close")).removeClass("zojs--on").trigger("close.zo"),!1});n(window).on("keyup",function(t){t.keyCode===27&&n(".zojs--on").removeClass("zojs--on")});n(document).on("open.zo",".zo-dlgContainer",function(){n(this).find(".zo-dlg").css("top",n(window).scrollTop())})})}(jQuery)
+/// <reference path="jquery/jquery-1.10.2.min.js"/>
+(function ($) {
+    // Later release the polyfill in case jQuery is not yet loaded
+    $(function () {
+        $(document).on("click", "[data-zo-navbar]", function () {
+            $($(this).attr("data-zo-navbar")).toggleClass("zojs-navbar--on");
+            return false;
+        });
+    });
+})(window.jQuery);
+/// <reference path="jquery/jquery-1.10.2.min.js"/>
+(function ($) {
+    $(function () {
+        $(document).on("click", "[data-zo-toggle]", function () {
+            var el = $($(this).attr("data-zo-toggle"));
+            if (el.hasClass("zojs--on")) {
+                el.removeClass("zojs--on").trigger("close.zo");
+            } else {
+                el.addClass("zojs--on").trigger("open.zo");
+            }
+            return false;
+        });
+        $(document).on("click", "[data-zo-open]", function () {
+            $($(this).attr("data-zo-open")).addClass("zojs--on").trigger("open.zo");
+            return false;
+        });
+        $(document).on("click", "[data-zo-close]", function () {
+            $($(this).attr("data-zo-close")).removeClass("zojs--on").trigger("close.zo");
+            return false;
+        });
+        $(window).on("keyup", function (e) {
+            if (e.keyCode === 27) {
+                $(".zojs--on").removeClass("zojs--on");
+            }
+        });
+        $(document).on("open.zo", ".zo-dlgContainer", function () {
+            $(this).find(".zo-dlg").css("top", $(window).scrollTop());
+        });
+    });
+})(jQuery);
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+"use strict";
+var gulp = require('gulp'),
+    rename = require("gulp-rename"),
+    concat = require("gulp-concat"),
+    uglify = require("gulp-uglify"),
+    cssImport = require("gulp-import-css"),
+    minifyCSS = require("gulp-minify-css"),
+    jsDest = "build/js/",
+    cssDest = "build/css/";
+
+gulp.task('js', function () {
+    // place code for your default task here
+    return gulp.src(["js/nav.js", "js/toggle.js"])
+        .pipe(concat("zoaui.js"))
+        .pipe(gulp.dest(jsDest))
+        .pipe(uglify())
+        .pipe(rename("zoaui.min.js"))
+        .pipe(gulp.dest(jsDest));
+});
+gulp.task('css', function () {
+    // place code for your default task here
+    return gulp.src("css/imports/imports.css")
+        .pipe(cssImport())
+        .pipe(rename("zoaui.css"))
+        .pipe(gulp.dest(cssDest))
+        .pipe(minifyCSS())
+        .pipe(rename("zoaui.min.css"))
+        .pipe(gulp.dest(cssDest));
+    //.pipe(uglify())
+    //.pipe(rename("zoaui.min.js"))
+    //.pipe(gulp.dest(cssDest));
+});
+gulp.task("default", ["js", "css"]);
